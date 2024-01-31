1926a60bf903567e0cbe21066f73802ba7a8016c
Daniela Lopez <174199@mail.unacar.mx>
2024-01-31 02:32:02 -0600

    a punto de iniciar con lo bueno jejeje

--- tv-search/src/components/SearchBar.jsx
+++ tv-search/src/components/SearchBar.jsx
@@ -24,9 +24,10 @@ const SearchBar = () => {
         <input
             className="form-control me-2"
             type="search"
-            placeholder="Looking for something?"
+            placeholder="Looking for something to watch?"
             aria-label="Search"
             value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
         />
         <button className="btn btn-outline-warning" type="submit" onClick={onSearchHandler}>Search</button>
     </form>
